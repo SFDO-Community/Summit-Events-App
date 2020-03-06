906c587fc29e0ea9c0f66082e63654986b9f28ce
Thaddaeus Dahlberg <tcdahlberg@users.noreply.github.com>
2020-03-06 14:31:58 -0600

    fixed Hard matching contact creation with no duplicate management bug were null id was passed

--- force-app/main/default/staticresources/SummitEventsAssets/js/options.js
+++ force-app/main/default/staticresources/SummitEventsAssets/js/options.js
@@ -6,103 +6,110 @@ var appointmentsReady = (callback) => {
 }
 
 appointmentsReady(() => {
-    document.querySelectorAll("#chooser .appointmentAdd").forEach( apptClick => {
+    let chooser = document.querySelector("#chooser");
+    let allApptAddButtons = chooser.querySelectorAll(".appointmentAdd");
+    allApptAddButtons.forEach(function (apptButton) {
+        apptButton.addEventListener("click", (addAppt) => {
+            console.log('Add Appointment fired');
+            let Appt = apptButton.closest(".appointment");
+            let requiredSel = Appt.querySelectorAll('.required');
+            let error = false;
+            if (requiredSel.length > 0) {
+                if (requiredSel.value === '') {
+                    error = true;
+                    requiredSel.classList.add('aptError');
+                    requiredSel.addEventListener('change', (err) => {
+                        err.classList.remove("aptError");
+                    });
+                }
+            }
 
-        let addAppt;
-        let Appt = apptClick.closest(".appointment");
-        let requiredSel = Appt.querySelectorAll('.required');
-        let error = false;
-        if (requiredSel.length > 0) {
-            if (requiredSel.value === '') {
-                error = true;
-                requiredSel.classList.add('aptError');
-                requiredSel.addEventListener('change', (err) => {
-                    err.classList.remove("aptError");
+            if (!error) {
+                //move and adjust data
+                let choosenArea = document.getElementById("choosen");
+                let chooserArea = document.getElementById("chooser");
+                let limit = Appt.dataset.limit;
+                limit--;
+
+                addAppt = document.createElement('div');
+                addAppt.classList.add('slds-box', 'slds-box_small', 'slds-m-vertical_x-small', 'appointmentChoosen');
+                console.log(addAppt.dataset);
+                addAppt.dataset= Appt.dataset;
+
+                let appTitle = document.createElement('p');
+                addAppt.classList.add('appointmentTitle');
+                let findTitle = Appt.querySelector(".appointmentTitle");
+                appTitle.textContent = findTitle.textContent;
+                addAppt.append(appTitle);
+
+                if (Appt.querySelectorAll(".appointmentType").length > 0) {
+                    let apptType = document.createElement('p');
+                    apptType.classList.add('appointmentType');
+                    let selType = Appt.querySelector(".appointmentType");
+                    apptType.textContent = selType.options[selType.selectedIndex].value;
+                    addAppt.append(apptType);
+                }
+
+                if (Appt.querySelectorAll(".appointmentCustomInput").length > 0) {
+                    let apptDesc = document.createElement('p');
+                    apptDesc.classList.add('appointmentDesc');
+                    apptDesc.textContent = Appt.querySelector(".appointmentCustomInput").value;
+                    addAppt.append(apptDesc);
+                }
+
+                //Create the remove button
+                let removeButton = document.createElement('a');
+                removeButton.classList.add('appointmentRemove');
+                removeButton.textContent = ' Remove ';
+                removeButton.addEventListener("click", function () {
+                    let rmvAppt = removeButton.closest(".appointmentChoosen");
+                    let origId = rmvAppt.dataset.origId;
+                    let origAppt = chooserArea.querySelector('#' + origId);
+                    origAppt.style.display = "block";
+                    origAppt.dataset.limit = removeButton
+                    choosenArea.removeChild(rmvAppt);
+                    // let apptChoosen = docuemnt.querySelector('#chooser');
+                    // let appt = removeButton.closest(".appointmentChoosen");
+                    // let chooseId = apptChoose.origId;
+                    // var appChooser = $('#' + aptChoosen.data('apptid'));
+                    // appChooser.data('limit', appChooser.data('limit') + 1);
+                    // appChooser.fadeIn("fast");
+                    // aptChoosen.remove();
+
+
+                    // removeAppt(removeButton);
                 });
+                addAppt.appendChild(removeButton);
+
+                Appt.dataset.limit = limit;
+
+
+                addAppt.id = 'appt-' + Appt.id;
+                addAppt.dataset.origId = Appt.id;
+
+
+                choosenArea.append(addAppt);
+
+                if (Appt.dataset.limit < 1) {
+                    Appt.style.display = "none";
+                }
+
             }
-        }
-
-        if (!error) {
-
-            // //build a box
-            // var ApptOffset = Appt.offset();
-            // var lc;
-            // var movingBox = $("<div/>", {id: 'movingBox' + Appt.attr("id"), class: 'slds-box slds-box_small slds-theme_shade'});
-            // movingBox.css({
-            //     "width": (Appt.width() + 1) + 'px',
-            //     "height": (Appt.height() + 1) + 'px',
-            //     "left": ApptOffset.left + 'px',
-            //     "top": ApptOffset.top + 'px',
-            //     "position": "absolute"
-            // });
-            // movingBox.html('<p>Adding Appointment...</p>')
-            // $('body').before(movingBox);
-            // if ($("#choosen .appointmentChoosen:last-child").length > 0) {
-            //     lc = $("#choosen .appointmentChoosen:last-child");
-            // } else {
-            //     lc = $("#choosen");
-            // }
-            // var lcOffset = lc.offset();
-            // movingBox.delay(300).animate({'top': (lcOffset.top + lc.height() + 10) + 'px', 'left': lcOffset.left + 'px', "height": "0px", "opacity": 0}, 300).fadeOut(100, function () {
-            //     $(this).remove();
-            // });
-
-            //move and adjust data
-            var limit = Appt.dataset.limit;
-
-            limit--;
-
-            addAppt = document.createElement('div');
-            addAppt.classList.add('slds-box', 'slds-box_small', 'slds-m-vertical_x-small');
-
-            let appTitle = document.createElement('p');
-            addAppt.classList.add('appointmentTitle');
-            let findTitle = Appt.querySelector(".appointmentTitle");
-            appTitle.textContent = findTitle.textContent;
-            addAppt.append(appTitle);
-
-            if (Appt.querySelectorAll(".appointmentType").length > 0) {
-                let apptType = document.createElement('p');
-                apptType.classList.add('appointmentType');
-                let selType = Appt.querySelector(".appointmentType");
-                apptType.textContent = selType.options[selType.selectedIndex].value;
-                addAppt.append(apptType);
-            }
-
-            if (Appt.querySelectorAll(".appointmentCustomInput").length > 0) {
-                let apptDesc = document.createElement('p');
-                apptDesc.classList.add('appointmentDesc');
-                apptDesc.textContent = Appt.querySelector(".appointmentCustomInput").value;
-                addAppt.append(apptDesc);
-            }
-
-            addAppt.append(
-                $('<a/>', {class: "appointmentRemove"})
-                    .html(' Remove ')
-                    .on("click", function () {
-                        removeAppt($(this));
-                    })
-            );
-
-            Appt.dataset.limit = limit;
-
-            addAppt.dataset = Appt.dataset;
-            addAppt.id = 'appt-' + Appt.id;
-
-            let choosenArea = document.getElementById("choosen");
-            choosenArea.append(addAppt)
-
-        }
+        });
     });
 
 
-    $(".slds-accordion__summary-heading").on("click", function () {
-        let section = $(this).closest('section');
-        if (section.hasClass('slds-is-open')) {
-            section.removeClass('slds-is-open')
-        } else {
-            section.addClass('slds-is-open')
-        }
+    let accordionHeads = document.querySelectorAll(".slds-accordion__summary-heading");
+    accordionHeads.forEach(function (ab) {
+        ab.addEventListener("click", function () {
+                let section = ab.closest('.appointment');
+                if (section.classList.contains('slds-is-open')) {
+                    section.classList.remove('slds-is-open')
+                } else {
+                    section.classList.add('slds-is-open')
+                }
+            }
+        );
     });
 
 });
