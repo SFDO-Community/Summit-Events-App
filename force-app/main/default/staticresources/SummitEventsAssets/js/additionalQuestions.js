bebaa47344010871dc59d0cfe62dccbef5af90b9
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2021-07-14 19:17:03 -0500

    html5 slds error help text validates on Additional Questions

--- force-app/main/default/staticresources/SummitEventsAssets/js/additionalQuestions.js
+++ force-app/main/default/staticresources/SummitEventsAssets/js/additionalQuestions.js
@@ -4,5 +4,20 @@ let ready = (callback) => {
 }
 
 ready(() => {
-
+    const questionForm = document.querySelector("[id$=SummitEventQuestionForm]");
+    questionForm.addEventListener('invalid', (e) => {
+        e.preventDefault();
+        let allInputs = document.querySelectorAll('input, select, textarea');
+        allInputs.forEach(input => {
+            if (!input.validity.valid) {
+                fadein();
+                input.setAttribute('aria-invalid', 'true')
+                input.closest('.slds-form-element').classList.add('slds-has-error');
+                input.addEventListener('change', () => {
+                    input.closest('.slds-form-element').classList.remove('slds-has-error');
+                    input.removeAttribute('aria-invalid')
+                });
+            }
+        });
+    }, true)
 });
