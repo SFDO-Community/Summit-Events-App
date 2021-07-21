e6b15262c5e03f0632a32fdb6c32cfd71733c7dd
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2021-07-21 10:05:40 -0500

    School account snowfakery, checkbox add questions, stub for record lookup

--- force-app/main/default/staticresources/SummitEventsAssets/js/sldsHelper.js
+++ force-app/main/default/staticresources/SummitEventsAssets/js/sldsHelper.js
@@ -7,6 +7,7 @@ readySLDS(() => {
     activateTooltips();
     activateHelpButton();
     adjustLabelsFor();
+    activateAutoComplete();
 });
 
 /* Tooltip */
@@ -57,8 +58,8 @@ function activateHelpButton() {
 
 function adjustLabelsFor() {
 
-    document.querySelectorAll('.slds-input, .slds-select, .slds-textarea').forEach(inputFound => {
-        let inputWrapper = inputFound.closest('.slds-form-element')
+    document.querySelectorAll('.slds-input, .slds-select, .slds-textarea, .slds-checkbox input').forEach(inputFound => {
+        let inputWrapper = inputFound.closest('.slds-form-element');
         let inputLabel = inputWrapper.querySelector('label')
         let helpText = inputWrapper.querySelector('.slds-form-element__help');
 
@@ -70,6 +71,17 @@ function adjustLabelsFor() {
                 inputLabel.htmlFor = inputFound.getAttribute('id');
             }
         }
+
+        if (inputFound.classList.contains('slds-checkbox')) {
+            inputLabel.addEventListener('click', function (e) {
+                if (inputFound.getAttribute('checked')) {
+                    inputFound.removeAttribute('checked')
+                } else {
+                    inputFound.setAttribute('checked', 'checked');
+                }
+            });
+        }
+
         if (inputFound && helpText) {
             if (helpText) {
                 inputFound.setAttribute('aria-describedby', helpText.getAttribute('id'));
@@ -84,4 +96,138 @@ function adjustLabelsFor() {
             }
         }
     });
+}
+
+function activateAutoComplete() {
+
+    document.querySelectorAll('.bind-autocomplete').forEach(autoItem => {
+
+        let originObjId = autoItem.id;
+        let comboBoxContainer = autoItem.closest('.slds-combobox_container');
+        let hiddenInput = comboBoxContainer.querySelector('.inputHidden');
+        let comboBox = comboBoxContainer.querySelector('.slds-combobox');
+        let objectType = comboBox.dataset.objtype;
+        let objectTypeFilter = comboBox.dataset.objtypefilter;
+        let objectTypeNameField = comboBox.dataset.objtypenamefield;
+        let removeButton = comboBox.querySelector('.refRemoveButton');
+        let magGlass = comboBox.querySelector('.refMagGlass');
+        let resultList = comboBox.querySelector('.slds-listbox');
+
+        /* Remote reference lookup */
+        const resultListTemplate = (title, subtitle, icon, originObjId, resultId) => `
+            <li role="presentation" class="slds-listbox__item" data-title="${title} ${subtitle}" data-origId="${originObjId}" data-resultId="${resultId}">
+                <div id="option1" class="slds-media slds-listbox__option slds-listbox__option_entity slds-listbox__option_has-meta" role="option">
+                  <span class="slds-media__figure slds-listbox__option-icon">
+                    <span class="slds-icon_container slds-icon-standard-account">
+                      <svg class="slds-icon slds-icon_small" aria-hidden="true">
+                        <use xlink:href="${icon}"></use>
+                      </svg>
+                    </span>
+                  </span>
+                  <span class="slds-media__body">
+                    <span class="slds-listbox__option-text slds-listbox__option-text_entity">${title}</span>
+                    <span class="slds-listbox__option-meta slds-listbox__option-meta_entity">${subtitle}</span>
+                  </span>
+                </div>
+            </li>
+        `;
+
+        function refValueAdded() {
+            comboBox.classList.remove('slds-is-open');
+            autoItem.classList.add('slds-combobox__input-value');
+            comboBoxContainer.classList.add('slds-has-selection');
+            removeButton.style.display = 'inline-flex';
+            magGlass.style.display = 'none';
+        }
+
+        function refValueRemoved() {
+            comboBox.classList.remove('slds-is-open');
+            autoItem.classList.remove('slds-combobox__input-value');
+            comboBoxContainer.classList.remove('slds-has-selection');
+            removeButton.style.display = 'none';
+            magGlass.style.display = 'inline-flex';
+            autoItem.value = '';
+            hiddenInput.value = '';
+            resultList.innerHTML = '';
+        }
+
+        function lookupResultsFormatter(data, originObjId) {
+            let outputList = ''
+            let fieldNames = comboBox.dataset.objtypenamefield.replace(' ', '').split(',');
+            data.forEach(result => {
+                let resultName = '';
+                let subTitle = '';
+                let resultId = '';
+                if (result['Id']) {
+                    resultId = result['Id'];
+                }
+                for (let x = 0; x < fieldNames.length; x++) {
+                    let fieldName = fieldNames[x].trim();
+                    if (x === 0) {
+                        resultName = result[fieldName];
+                    } else {
+                        if (result[fieldName]) {
+                            subTitle += result[fieldName] + ', ';
+                        }
+                    }
+                }
+                if (subTitle) {
+                    subTitle = subTitle.substr(0, subTitle.length - 2);
+                }
+                outputList += resultListTemplate(resultName, subTitle, comboBox.dataset.listicon, originObjId, resultId);
+            });
+            resultList.innerHTML = '';
+
+            comboBox.classList.remove('slds-is-open');
+            if (outputList) {
+                comboBox.classList.add('slds-is-open');
+            }
+
+            resultList.insertAdjacentHTML("beforeend", outputList);
+
+            resultList.querySelectorAll('li').forEach(refItem => {
+                refItem.addEventListener('click', function (e) {
+                    if (refItem.dataset.title === '**createnew**') {
+                        if (typeof window['setCreatingNewRelatedRecordAF' + groupId] === "function" && recordId && resultId) {
+                            window['setCreatingNewRelatedRecordAF' + groupId](recordId, resultId)
+                            console.log('result function called!');
+                        }
+                    } else {
+                        hiddenInput.value = refItem.dataset.resultid;
+                        autoItem.value = refItem.dataset.title;
+                        refValueAdded();
+                    }
+                });
+            });
+        }
+
+        if (autoItem.value) {
+            refValueAdded();
+        }
+
+        autoItem.addEventListener('focusin', (e) => {
+            autoItem.classList.add('slds-has-focus');
+            comboBox.classList.add('slds-is-open');
+        });
+
+        removeButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            refValueRemoved();
+        });
+
+        autoItem.addEventListener('keyup', (e) => {
+            let searchTerm = autoItem.value;
+            if (objectType && objectTypeFilter && objectTypeNameField && searchTerm.length > 2) {
+                lookupSearchJS(objectType, objectTypeFilter, objectTypeNameField, searchTerm, lookupResultsFormatter, originObjId);
+            }
+        });
+
+
+        // comboBox.addEventListener('focusout', (e) => {
+        //     console.log('focus out of combo box');
+        //     autoItem.classList.remove('slds-has-focus');
+        //     comboBox.classList.remove('slds-is-open');
+        // });
+    });
+
 }
