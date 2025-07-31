285a0a7e18413389a2cb9ff784406053b7d0a769
Alex Kadis <akadis@salesforce.com>
2025-07-31 16:37:32 -0400

    Working version of Summit Events Registrant Map
    Can be added to either the Summit Event FlexiPage or Summit Event Instance FlexiPage after activating data integration rule for the mapping data service. Details: https://help.salesforce.com/s/articleView?id=sales.ddc_data_integration_rule_activating.htm&type=5

--- force-app/main/default/lwc/summitEventsRegistrantMap/summitEventsRegistrantMap.js
+++ force-app/main/default/lwc/summitEventsRegistrantMap/summitEventsRegistrantMap.js
@@ -1,36 +1,69 @@
 import { LightningElement, wire, api } from 'lwc';
 import getSummitEventsRegistrants from '@salesforce/apex/SummitEventsMapController.getSummitEventsRegistrants';
+import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 
 export default class SummitEventsRegistrantMap extends LightningElement {
 	@api recordId;
     @api objectApiName;
     mapMarkers;
+	selectedEventInstance = '';
+	selectedMarkerValue;
+	eventInstancesOptions = [
+		{ label: 'All Event Instances', value: '' }
+	];
 
-	// Define the center of the map
-    center = {
-        location: {
-            // Latitude: 44.949642, // San Francisco Latitude
-            // Longitude: -93.093124 // San Francisco Longitude
-        }
-    };
+	get selectedEventOrEventInstanceId() {
+		return (this.selectedEventInstance === null || this.selectedEventInstance === '') ? this.recordId : this.selectedEventInstance;
+	}
 
-	@wire(getSummitEventsRegistrants, {eventId: '$recordId'})
+	get isEvent() {
+		return this.objectApiName === 'Summit_Events__c';
+	}
+
+	@wire(getRelatedListRecords, {
+		parentRecordId: '$recordId',
+		relatedListId: 'Event_Instances__r',
+		fields: ['Summit_Events_Instance__c.Id','Summit_Events_Instance__c.Event__c', 'Summit_Events_Instance__c.Name'],
+		orderBy: 'Summit_Events_Instance__c.Name'
+	})
+	listInfo({ error, data }) {
+		if (data) {
+			data.records.forEach((childRecord) => {
+				this.eventInstancesOptions.push({
+					label: childRecord?.fields?.Name?.value,
+					value: childRecord?.fields?.Id?.value
+				})
+			});
+			this.eventInstancesOptions = JSON.parse(JSON.stringify(this.eventInstancesOptions));
+		} else if (error) {
+			if (this.isEvent) {
+				console.error(JSON.stringify(error));
+			}
+		}
+	}
+
+	@wire(getSummitEventsRegistrants, {eventId: '$selectedEventOrEventInstanceId'})
 	summitEventsRegistrants ({error, data}) {
 		if (error) {
-			console.error(error);
+			console.error(JSON.stringify(error));
 		} else if (data) {
-			console.log((JSON.stringify(data)));
 			this.mapMarkers = data.map((registrant) => ({
 				location: {
 					Latitude: registrant?.Contact__r?.MailingLatitude,
-					Longitude: registrant?.Contact__r?.MailingLongitude
+					Longitude: registrant?.Contact__r?.MailingLongitude,
 				},
-				title: registrant.Preferred_First_Name_Formatted__c || 'Registrant',
-				url: '/lightning/r/Account/' + registrant.Id + '/view',
-				description: 'Registration',
-				value: registrant.Id,
+				title: registrant?.Preferred_First_Name_Formatted__c || 'Registrant',
+				description: registrant?.Contact__r?.MailingCity + ', ' + (registrant?.Contact__r?.MailingState === undefined ? registrant?.Contact__r?.MailingCountry : registrant?.Contact__r?.MailingState),
+				value: registrant?.Id,
 				icon: 'standard:location'
 			}));
 		}
+	}
+	handleSelectEventInstanceChange(event) {
+		this.selectedEventInstance = event.target.value;
+	}
+
+	handleMarkerSelect(event) {
+        this.selectedMarkerValue = event.target.selectedMarkerValue;
 	}
 }
